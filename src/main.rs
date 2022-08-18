e2ea458568a6b0a1f11fc7e2aaa915a309da7d11
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2022-08-18 07:46:29 -0300

    Complete

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     loop {
-        println!("Please input your guess.");
+        println!("Please input your guess (1-100).");
 
         let mut guess = String::new();
 
@@ -18,7 +18,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
